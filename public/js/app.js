22cd34180132eca277b91dfe56073b669c6de6f7
InkSlob <jessewrigh@yahoo.com>
2016-06-22 17:02:21 -0400

    text edits

--- public/js/app.js
+++ public/js/app.js
@@ -114,7 +114,7 @@ angular.module("contactsApp", ['ngRoute'])
                 });
         }
     })
-	.controller("ListController", function(contacts, $scope) {
+	.controller("ListController", function(contacts,progressReports, $scope) {
         $scope.contacts = contacts.data;
 		$scope.progressReports = progressReports.data;
     })
