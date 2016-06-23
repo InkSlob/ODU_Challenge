db1b1b54579d1d8ae490e067356a47e0d706fc89
bharath kongara <bkongara@cs.odu.edu>
2016-06-23 10:37:09 -0400

    app.js update

--- public/js/app.js
+++ public/js/app.js
@@ -160,14 +160,11 @@ angular.module("contactsApp", ['ngRoute'])
         $scope.deleteProgress = function(progressId) {
             Contacts.deleteContact(progressId);
         }
-		
-		
     })
     .controller("NewContactController", function($scope, $location, Contacts) {
         $scope.back = function() {
             $location.path("#/");
         }
-
         $scope.saveContact = function(contact) {
             Contacts.createContact(contact).then(function(doc) {
                 var contactUrl = "/contact/" + doc.data._id;
@@ -177,9 +174,6 @@ angular.module("contactsApp", ['ngRoute'])
             });
         }
     })
-	// added 6/22  jesse
-	
-	// end add
     .controller("EditContactController", function($scope, $routeParams, Contacts) {
         Contacts.getContact($routeParams.contactId).then(function(doc) {
             $scope.contact = doc.data;
