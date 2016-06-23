e45d5aa5cd2da1a6faa2275e383b19e7439e27f6
bharath kongara <bkongara@cs.odu.edu>
2016-06-23 10:12:33 -0400

    app.js update

--- public/js/app.js
+++ public/js/app.js
@@ -37,14 +37,14 @@ angular.module("contactsApp", ['ngRoute'])
     })
 	 .service("Progress", function($http) {
 		 
-		   this.getProgressReports = function() {
-            return $http.get("/progress").
-                then(function(response) {
-                    return response;
-                }, function(response) {
-                    alert("Error finding contacts.");
-                });
-        }
+		   //this.getProgressReports = function() {
+           // return $http.get("/progress").
+          //      then(function(response) {
+          //          return response;
+         //       }, function(response) {
+          //          alert("Error finding contacts.");
+          //      });
+       // }
 		   this.createProgressReport = function(progressReport) {
             return $http.post("/progress", progressReport).
                 then(function(response) {
