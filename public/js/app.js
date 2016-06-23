e30a01aab7d16b85954273733ac496382ca00372
bharath kongara <bkongara@cs.odu.edu>
2016-06-23 11:54:59 -0400

    app.js update

--- public/js/app.js
+++ public/js/app.js
@@ -174,6 +174,19 @@ angular.module("contactsApp", ['ngRoute'])
             });
         }
     })
+	.controller("NewProgressController", function($scope, $location, Contacts) {
+        $scope.back = function() {
+            $location.path("#/");
+        }
+        $scope.saveContact = function(contact) {
+            Contacts.createContact(contact).then(function(doc) {
+                var contactUrl = "/contact/" + doc.data._id;
+                $location.path(contactUrl);
+            }, function(response) {
+                alert(response);
+            });
+        }
+    })
     .controller("EditContactController", function($scope, $routeParams, Contacts) {
         Contacts.getContact($routeParams.contactId).then(function(doc) {
             $scope.contact = doc.data;
